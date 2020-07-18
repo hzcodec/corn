f4fc2842ad2a0cd2f1fe824b00aa327d8cc83de0
heinz.samuelsson <heinz.samuelsson@unjo.com>
2020-07-18 23:54:42 +0200

    Mupp

--- io_ctrl_tool.py
+++ io_ctrl_tool.py
@@ -53,33 +53,6 @@ btnCalibRestartPos = (Xpos, Ypos+180)
 DELIMITER = 190*'.'
 
 
-#def list_serial_ports():
-#    system_name = platform.system()
-#    print system_name
-#
-#    if system_name == "Windows":
-#        # Scan for available ports.
-#        available = []
-#
-#        for i in range(256):
-#            try:
-#                s = serial.Serial(i)
-#                available.append(i)
-#                s.close()
-#            except serial.SerialException:
-#                pass
-#        return available
-#
-#    elif system_name == "Darwin":
-#        # Mac
-#        return glob.glob('/dev/tty*') + glob.glob('/dev/cu*')
-#
-#    else:
-#        # Assume Linux or something else
-#	print 'config_tool.py: list_serial_ports() - Serial ports scanned'
-#        return glob.glob('/dev/ttyA*') + glob.glob('/dev/ttyUSB*')
-
-
 def serial_cmd(cmd, serial):
     # send command to serial port
     serial.write(cmd+'\r');
@@ -129,200 +102,194 @@ class MyForm(wx.Frame):
 	self.CenterOnScreen()
         #self.panel.SetBackgroundColour((212, 170, 103))
 
-        img1 = wx.Image('logo.png', wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-	img2 = wx.ImageFromBitmap(img1)
         width = 80 
-        height = 40
-	img3 = img2.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
-	result = wx.BitmapFromImage(img3)
-        wx.StaticBitmap(self.panel, -1, result, (680, 15))
 
-	self.menuBar = wx.MenuBar()
-	self.fileMenu = wx.Menu()
+#	self.menuBar = wx.MenuBar()
+#	self.fileMenu = wx.Menu()
+#
+#	self.menuBar.Append(self.fileMenu, "Config")
+#	aboutMenuItem = self.fileMenu.Append(wx.ID_ABOUT, "About", "Information about the application")
+#	exitMenuItem = self.fileMenu.Append(wx.NewId(), "Exit", "Exit the application")
+#	self.Bind(wx.EVT_MENU, self.onClose, exitMenuItem)
+#	self.Bind(wx.EVT_MENU, self.onAbout, aboutMenuItem)
+#        self.SetMenuBar(self.menuBar)
+#
+#        self.defineButtons()
+#	self.defineCombo()
+#
+#	self.lblConnected = wx.StaticText(self.panel, label= '-----', pos=(390,35))
+#	self.lblConfigSaved = wx.StaticText(self.panel, label= '-----', pos=(Xpos+160,Ypos2+466))
+#
+#        self.lblDelimiter1 = wx.StaticText(self.panel, -1, DELIMITER, pos = (10,60))
+#        self.lblDelimiter1.SetForegroundColour(wx.Colour(0,0,255))
+#
+#        self.lblDelimiter2 = wx.StaticText(self.panel, -1, DELIMITER, pos = (10,210))
+#        self.lblDelimiter2.SetForegroundColour(wx.Colour(0,0,255))
+#
+#        self.lblDelimiter2 = wx.StaticText(self.panel, -1, DELIMITER, pos = (10,500))
+#        self.lblDelimiter2.SetForegroundColour(wx.Colour(0,0,255))
+#
+#	font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
+#	lblAscendVer = wx.StaticText(self.panel, label='Ascender version:', pos=(Xpos+5,Ypos2+17))
+#	lblAscendVer.SetFont(font)
+#	lblRemoteVer = wx.StaticText(self.panel, label='Remote version:', pos=(Xpos+5,Ypos2+77))
+#	lblRemoteVer.SetFont(font)
+#
+#	self.lblAlign = wx.StaticText(self.panel, label='Alignment not done!', pos=ALIGN_MSG)
+#	self.lblCalibRight = wx.StaticText(self.panel, label='Turn throttle handle max Up', pos=CALIB_MSG1)
+#	self.lblCalibLeft = wx.StaticText(self.panel, label='Turn throttle handle max Down', pos=CALIB_MSG2)
+#	self.lblCalibNormal = wx.StaticText(self.panel, label='Set throttle handle in neutral position', pos=CALIB_MSG3)
+#
+#	self.printPortName()
+#        logging.basicConfig(format="%(filename)s: %(funcName)s() - %(message)s", level=logging.INFO)
 
-	self.menuBar.Append(self.fileMenu, "Config")
-	aboutMenuItem = self.fileMenu.Append(wx.ID_ABOUT, "About", "Information about the application")
-	exitMenuItem = self.fileMenu.Append(wx.NewId(), "Exit", "Exit the application")
-	self.Bind(wx.EVT_MENU, self.onClose, exitMenuItem)
-	self.Bind(wx.EVT_MENU, self.onAbout, aboutMenuItem)
-        self.SetMenuBar(self.menuBar)
-
-        self.defineButtons()
-	self.defineCombo()
-
-	self.lblConnected = wx.StaticText(self.panel, label= '-----', pos=(390,35))
-	self.lblConfigSaved = wx.StaticText(self.panel, label= '-----', pos=(Xpos+160,Ypos2+466))
-
-        self.lblDelimiter1 = wx.StaticText(self.panel, -1, DELIMITER, pos = (10,60))
-        self.lblDelimiter1.SetForegroundColour(wx.Colour(0,0,255))
-
-        self.lblDelimiter2 = wx.StaticText(self.panel, -1, DELIMITER, pos = (10,210))
-        self.lblDelimiter2.SetForegroundColour(wx.Colour(0,0,255))
-
-        self.lblDelimiter2 = wx.StaticText(self.panel, -1, DELIMITER, pos = (10,500))
-        self.lblDelimiter2.SetForegroundColour(wx.Colour(0,0,255))
-
-	font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-	lblAscendVer = wx.StaticText(self.panel, label='Ascender version:', pos=(Xpos+5,Ypos2+17))
-	lblAscendVer.SetFont(font)
-	lblRemoteVer = wx.StaticText(self.panel, label='Remote version:', pos=(Xpos+5,Ypos2+77))
-	lblRemoteVer.SetFont(font)
-
-	self.lblAlign = wx.StaticText(self.panel, label='Alignment not done!', pos=ALIGN_MSG)
-	self.lblCalibRight = wx.StaticText(self.panel, label='Turn throttle handle max Up', pos=CALIB_MSG1)
-	self.lblCalibLeft = wx.StaticText(self.panel, label='Turn throttle handle max Down', pos=CALIB_MSG2)
-	self.lblCalibNormal = wx.StaticText(self.panel, label='Set throttle handle in neutral position', pos=CALIB_MSG3)
-
-	self.printPortName()
-        logging.basicConfig(format="%(filename)s: %(funcName)s() - %(message)s", level=logging.INFO)
-
-    def defineButtons(self):
-        self.btnClose = wx.Button(self.panel, label="Close",  pos=btnClosePos, size=(110, 30))
-        self.btnSaveConfig = wx.Button(self.panel, label="Save Configuration",  pos=btnSavePos, size=(150, 30))
-        self.btnConnect = wx.Button(self.panel, label="Connect",  pos=btnConnectPos, size=(110, 30))
-        self.btnAlign = wx.Button(self.panel, label="Align",  pos=btnAlignPos, size=(110, 30))
-
-        self.btnCalibRight = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Right", name="calibr", pos=btnCalibRightPos, size=(110, 30))
-        self.btnCalibLeft = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Left", name="calibl", pos=btnCalibLeftPos, size=(110, 30))
-        self.btnCalibNormal = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Neutral", name="calibn", pos=btnCalibNormalPos, size=(110, 30))
-        self.btnCalibRestart = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Restart", name="calibr", pos=btnCalibRestartPos, size=(110, 30))
-	self.btnCalibLeft.Enable(False)
-	self.btnCalibNormal.Enable(False)
-
-	self.bindButtons()
-
-	Publisher.subscribe(self.aligned_finished, "topic_aligned")
-	 
-
-    def bindButtons(self):
-	self.Bind(wx.EVT_BUTTON, self.onClose, id=self.btnClose.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onSaveConfiguration, id=self.btnSaveConfig.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onConnect, id=self.btnConnect.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onAlign, id=self.btnAlign.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onCalibRight, id=self.btnCalibRight.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onCalibLeft, id=self.btnCalibLeft.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onCalibNormal, id=self.btnCalibNormal.GetId())
-	self.Bind(wx.EVT_BUTTON, self.onCalibRestart, id=self.btnCalibRestart.GetId())
+#    def defineButtons(self):
+#        self.btnClose = wx.Button(self.panel, label="Close",  pos=btnClosePos, size=(110, 30))
+#        self.btnSaveConfig = wx.Button(self.panel, label="Save Configuration",  pos=btnSavePos, size=(150, 30))
+#        self.btnConnect = wx.Button(self.panel, label="Connect",  pos=btnConnectPos, size=(110, 30))
+#        self.btnAlign = wx.Button(self.panel, label="Align",  pos=btnAlignPos, size=(110, 30))
+#
+#        self.btnCalibRight = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Right", name="calibr", pos=btnCalibRightPos, size=(110, 30))
+#        self.btnCalibLeft = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Left", name="calibl", pos=btnCalibLeftPos, size=(110, 30))
+#        self.btnCalibNormal = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Neutral", name="calibn", pos=btnCalibNormalPos, size=(110, 30))
+#        self.btnCalibRestart = wx.Button(self.panel, id=wx.ID_ANY, label="Calib Restart", name="calibr", pos=btnCalibRestartPos, size=(110, 30))
+#	self.btnCalibLeft.Enable(False)
+#	self.btnCalibNormal.Enable(False)
+#
+#	self.bindButtons()
+#
+#	Publisher.subscribe(self.aligned_finished, "topic_aligned")
+#	 
+#
+#    def bindButtons(self):
+#	self.Bind(wx.EVT_BUTTON, self.onClose, id=self.btnClose.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onSaveConfiguration, id=self.btnSaveConfig.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onConnect, id=self.btnConnect.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onAlign, id=self.btnAlign.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onCalibRight, id=self.btnCalibRight.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onCalibLeft, id=self.btnCalibLeft.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onCalibNormal, id=self.btnCalibNormal.GetId())
+#	self.Bind(wx.EVT_BUTTON, self.onCalibRestart, id=self.btnCalibRestart.GetId())
     
 
-    def defineCombo(self):
-        portNames = ['ACM0', 'ACM1', 'USB0']
-        self.combo = wx.ComboBox(self.panel, choices=portNames, pos=(140, 27))
-        self.combo.SetSelection(0) # preselect ACM0
-        self.combo.Bind(wx.EVT_COMBOBOX, self.onCombo)
+#    def defineCombo(self):
+#        portNames = ['ACM0', 'ACM1', 'USB0']
+#        self.combo = wx.ComboBox(self.panel, choices=portNames, pos=(140, 27))
+#        self.combo.SetSelection(0) # preselect ACM0
+#        self.combo.Bind(wx.EVT_COMBOBOX, self.onCombo)
+#    
+#
+#    def printPortName(self):
+#	font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
+#	lblHead1 = wx.StaticText(self.panel, label='Serial Port:', pos=(Xpos+5,35))
+#	lblHead1.SetFont(font)
+#
+#        #ports = list_serial_ports()
+#	#port2 = "".join(ports)
+#	#port3 = port2[5:]
+#	#font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+#	#lblHead1 = wx.StaticText(self.panel, label=port3, pos=(Xpos+140,Ypos2-15))
+#	#lblHead1.SetFont(font)
+#
+#    
+#    def getVersion(self):
+#        #ver = serial_cmd('v', self.ser)
+#	lblAscenderVersion = wx.StaticText(self.panel, label= ver[1:], pos=(150,Ypos2))
+# 
+#        #ver = serial_cmd('r_v', self.ser)
+#	lblRemoteVersion = wx.StaticText(self.panel, label= ver[3:], pos=(150,Ypos2+60))
     
 
-    def printPortName(self):
-	font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-	lblHead1 = wx.StaticText(self.panel, label='Serial Port:', pos=(Xpos+5,35))
-	lblHead1.SetFont(font)
-
-        #ports = list_serial_ports()
-	#port2 = "".join(ports)
-	#port3 = port2[5:]
-	#font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
-	#lblHead1 = wx.StaticText(self.panel, label=port3, pos=(Xpos+140,Ypos2-15))
-	#lblHead1.SetFont(font)
-
-    
-    def getVersion(self):
-        ver = serial_cmd('v', self.ser)
-	lblAscenderVersion = wx.StaticText(self.panel, label= ver[1:], pos=(150,Ypos2))
- 
-        ver = serial_cmd('r_v', self.ser)
-	lblRemoteVersion = wx.StaticText(self.panel, label= ver[3:], pos=(150,Ypos2+60))
-    
-
-    def onClose(self, event):
-        self.Close()
-
-    def onAbout(self, event):
-        info = wx.AboutDialogInfo()
-        info.Name = "Configuration Tool"
-        info.Version = "0.1 Beta"
-	info.Description = "The tool is used to configure Actsafe's Ascender ACX/TCX"
-        info.Developers = ["Heinz Samuelsson, Unjo AB"]
-        wx.AboutBox(info)
-
-
-    def onSaveConfiguration(self, event):
-	now = datetime.datetime.now().strftime("%Y-%m-%d   %H:%M")
-	self.lblConfigSaved.SetLabel('Configuration saved at: ' + str(now))
-
-
-    def onConnect(self, event):
-	try:
-            self.ser = serial.Serial(port = '/dev/tty'+self.combo.GetValue(),
-                                     baudrate = 9600,
-                                     parity = serial.PARITY_NONE,
-                                     stopbits = serial.STOPBITS_ONE,
-                                     bytesize = serial.EIGHTBITS,
-                                     timeout = 1)
-
-            self.getVersion()
-	    self.lblConnected.SetLabel(self.combo.GetValue() + ' connected')
-            self.lblConnected.SetForegroundColour(wx.Colour(0,0,255))
-
-	except:
-	    self.lblConnected.SetLabel('Cannot connect ' + self.combo.GetValue())
-            self.lblConnected.SetForegroundColour(wx.Colour(255,0,0))
-	    print 'Error, could not open port ' + self.combo.GetValue()
-
-
-    def onAlign(self, event):
-        logging.info('')
-	PollAlignment(self.ser)
-        ver = serial_cmd('align', self.ser)
-	print ver
-
-	self.lblAlign.SetForegroundColour(BLUE)
-	self.lblAlign.SetLabel("Alignment initiated")
-	self.lblConfigSaved.SetLabel(' ')
-	self.btnAlign.Enable(False)
-	self.btnCalibRight.Enable(False)
-	self.btnCalibRestart.Enable(False)
-
-
-    def onCalibRight(self, event):
-        print 'Calib right'
-	self.btnCalibRight.Enable(False)
-	self.btnCalibLeft.Enable(True)
-	self.lblAlign.SetForegroundColour(GREEN)
-	self.lblCalibRight.SetLabel("Up Calibration finished")
-
-
-    def onCalibLeft(self, event):
-        print 'Calib left'
-	self.btnCalibLeft.Enable(False)
-	self.btnCalibNormal.Enable(True)
-	self.lblCalibLeft.SetLabel("Down Calibration finished")
-
-
-    def onCalibNormal(self, event):
-        print 'Calib normal'
-	self.btnCalibNormal.Enable(False)
-	self.lblCalibNormal.SetLabel("Neutral Calibration finished")
-
-
-    def onCalibRestart(self, event):
-	self.btnCalibRight.Enable(True)
-	self.lblConfigSaved.SetLabel(' ')
-	self.lblCalibRight.SetLabel("Turn throttle handle max Up")
-	self.lblCalibLeft.SetLabel("Turn throttle handle max Down")
-	self.lblCalibNormal.SetLabel("Set throttle handle in neutral position")
-
-
-    def onCombo(self, event):
-        print 'Selected port: ' + self.combo.GetValue()
-	#self.label.SetLabel("selected "+ self.combo.GetValue() +" from Combobox") 
-        #print('Connected to: ' + self.ser.portstr)
-
-    def aligned_finished(self):
-	self.btnAlign.Enable(True)
-	self.lblAlign.SetForegroundColour(GREEN)
-	self.lblAlign.SetLabel("Alignment finished.")
-	self.btnCalibRight.Enable(True)
-	self.btnCalibRestart.Enable(True)
+#    def onClose(self, event):
+#        self.Close()
+#
+#    def onAbout(self, event):
+#        info = wx.AboutDialogInfo()
+#        info.Name = "Configuration Tool"
+#        info.Version = "0.1 Beta"
+#	info.Description = "The tool is used to configure Actsafe's Ascender ACX/TCX"
+#        info.Developers = ["Heinz Samuelsson, Unjo AB"]
+#        wx.AboutBox(info)
+#
+#
+#    def onSaveConfiguration(self, event):
+#	now = datetime.datetime.now().strftime("%Y-%m-%d   %H:%M")
+#	self.lblConfigSaved.SetLabel('Configuration saved at: ' + str(now))
+#
+#
+#    def onConnect(self, event):
+#	try:
+#            self.ser = serial.Serial(port = '/dev/tty'+self.combo.GetValue(),
+#                                     baudrate = 9600,
+#                                     parity = serial.PARITY_NONE,
+#                                     stopbits = serial.STOPBITS_ONE,
+#                                     bytesize = serial.EIGHTBITS,
+#                                     timeout = 1)
+#
+#            self.getVersion()
+#	    self.lblConnected.SetLabel(self.combo.GetValue() + ' connected')
+#            self.lblConnected.SetForegroundColour(wx.Colour(0,0,255))
+#
+#	except:
+#	    self.lblConnected.SetLabel('Cannot connect ' + self.combo.GetValue())
+#            self.lblConnected.SetForegroundColour(wx.Colour(255,0,0))
+#	    print 'Error, could not open port ' + self.combo.GetValue()
+#
+#
+#    def onAlign(self, event):
+#        logging.info('')
+#	PollAlignment(self.ser)
+#        ver = serial_cmd('align', self.ser)
+#	print ver
+#
+#	self.lblAlign.SetForegroundColour(BLUE)
+#	self.lblAlign.SetLabel("Alignment initiated")
+#	self.lblConfigSaved.SetLabel(' ')
+#	self.btnAlign.Enable(False)
+#	self.btnCalibRight.Enable(False)
+#	self.btnCalibRestart.Enable(False)
+#
+#
+#    def onCalibRight(self, event):
+#        print 'Calib right'
+#	self.btnCalibRight.Enable(False)
+#	self.btnCalibLeft.Enable(True)
+#	self.lblAlign.SetForegroundColour(GREEN)
+#	self.lblCalibRight.SetLabel("Up Calibration finished")
+#
+#
+#    def onCalibLeft(self, event):
+#        print 'Calib left'
+#	self.btnCalibLeft.Enable(False)
+#	self.btnCalibNormal.Enable(True)
+#	self.lblCalibLeft.SetLabel("Down Calibration finished")
+#
+#
+#    def onCalibNormal(self, event):
+#        print 'Calib normal'
+#	self.btnCalibNormal.Enable(False)
+#	self.lblCalibNormal.SetLabel("Neutral Calibration finished")
+#
+#
+#    def onCalibRestart(self, event):
+#	self.btnCalibRight.Enable(True)
+#	self.lblConfigSaved.SetLabel(' ')
+#	self.lblCalibRight.SetLabel("Turn throttle handle max Up")
+#	self.lblCalibLeft.SetLabel("Turn throttle handle max Down")
+#	self.lblCalibNormal.SetLabel("Set throttle handle in neutral position")
+#
+#
+#    def onCombo(self, event):
+#        print 'Selected port: ' + self.combo.GetValue()
+#	#self.label.SetLabel("selected "+ self.combo.GetValue() +" from Combobox") 
+#        #print('Connected to: ' + self.ser.portstr)
+#
+#    def aligned_finished(self):
+#	self.btnAlign.Enable(True)
+#	self.lblAlign.SetForegroundColour(GREEN)
+#	self.lblAlign.SetLabel("Alignment finished.")
+#	self.btnCalibRight.Enable(True)
+#	self.btnCalibRestart.Enable(True)
 
 
 if __name__ == "__main__":
